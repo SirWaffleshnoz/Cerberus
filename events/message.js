cc2f1044084b313b4224cbe74fcd32d96a9b27f4
Jared Lyon <jaredalyon@gmail.com>
2021-01-03 16:32:28 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -21,40 +21,9 @@ exports.run = (bot, msg) => {
 
 	const msg1 = msg.content.toLowerCase();
 	//includes
-	if (msg1.includes("uwu")) {
-		msg.channel.send("uwu")
+	/**
+	if (msg1.includes("test")) {
+		msg.channel.send("test")
 	}
-	if (msg1.includes("owo")) {
-		msg.channel.send("owo")
-	}
-	if (msg1.includes(";w;")) {
-		msg.channel.send(";w;")
-	}
-	if (msg1.includes("ewe")) {
-		msg.channel.send("ewe")
-	}
-	if (msg1.includes("ayuh")) {
-		msg.channel.send("aYUH!!!")
-	}
-	if (msg1.includes("play despacito")) {
-		msg.channel.send("`Playing 'Despacito' by Luis Fonsi...`\nhttps://www.youtube.com/watch?v=kJQP7kiw5Fk")
-	}
-	if (msg1.includes("hewwo")) {
-		msg.channel.send("(;•́︿•̀ ;) Hewwo??")
-	}
-
-	//score writes
-	if (!bot.score[msg.author.id] && msg.channel.id != '479903283390185477') {
-		var roll = Math.floor((Math.random() * 20));
-		bot.score[msg.author.id].score += roll;
-		bot.score[msg.author.id].lastMessage = new Date();
-		console.log("User score logged!");
-	} else {
-		if (new Date() - new Date(bot.score[msg.author.id].lastMessage) >= 60000 && msg.channel.id != '479903283390185477') {
-			var roll = Math.floor((Math.random() * 20));
-			bot.score[msg.author.id].score += roll;
-			bot.score[msg.author.id].lastMessage = new Date();
-			console.log("User score logged!");
-		}
-	}
+	*/
 }
